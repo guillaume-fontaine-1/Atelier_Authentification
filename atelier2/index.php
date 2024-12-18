38ca421d5e93f72754c2331e4bb2aea18476e5cb
Guillaume Fontaine <gfontaine6@myges.fr>
2024-12-18 10:49:59 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -17,9 +17,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Vérification simple du username et de son password.
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
+    $jeton = bin2hex(random_bytes(16));
     if ($username === 'admin' && $password === 'secret') {
-        $jeton = bin2hex(random_bytes(16));
-        setcookie('authToken', $jeton, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        setcookie('authToken', "12345" . $jeton, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
